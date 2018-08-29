86bd0c68aa2c87844f7987b0c0565ed6762e5544
KhoaND290391 <nature.myloves@gmail.com>
2018-08-29 22:55:06 +0700

    Update using css external

--- src/components/home/FooterSection.tsx
+++ src/components/home/FooterSection.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { ComponentBase } from "../ComponentBase";
+import './HomeStyles.css';
 export class FooterSection extends ComponentBase<any, any> {
     render() {
         return (
