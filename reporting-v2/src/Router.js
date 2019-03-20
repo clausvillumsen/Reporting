607d34c218cebb947eeeb5b5337156bbf13d69d1
Lưu Bình An <anlb@blockchainlabs.asia>
2019-03-20 14:23:46 +0700

    update header

--- reporting-v2/src/Router.js
+++ reporting-v2/src/Router.js
@@ -4,20 +4,31 @@ import {
   Route,
   BrowserRouter as Router
 } from 'react-router-dom';
+import styled from 'styled-components';
 import Loading from './components/Loading';
-import Home from './screens/Home';
+// import Home from './screens/Home';
+import Header from './components/Header';
 
-// const Home = React.lazy(() => import('./screens/Home'));
+const Home = React.lazy(() => import('./screens/Home'));
+
+const Master = styled.div`
+  padding-top: 60px;
+`;
 
 
 const AppRouter = () => {
   return (
     <Router>
       <div id="app-router">
-        <Loading />
-        <Switch>
-          <Route path='/' exact component={(props) => <Home {...props}/>} />
-        </Switch>
+        <Master>
+          <Loading />
+          <Header />
+          <React.Suspense fallback={<Loading />}>
+            <Switch>
+              <Route path='/' exact component={(props) => <Home {...props}/>} />
+            </Switch>
+          </React.Suspense>
+        </Master>
       </div>
     </Router>
   )
