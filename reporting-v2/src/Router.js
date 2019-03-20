859ce1c3b34c41f95138822c84b00b671bf2b544
Lưu Bình An <anlb@blockchainlabs.asia>
2019-03-20 15:49:28 +0700

    update route

--- reporting-v2/src/Router.js
+++ reporting-v2/src/Router.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
-import styled from "styled-components";
-import Loading from "./components/Loading";
-// import Home from './screens/Home';
-import Header from "./components/Header";
+import React from 'react';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import styled from 'styled-components';
+import Loading from './components/Loading';
+import Home from './screens/Home';
+//import Header from "./components/Header";
 
-const Home = React.lazy(() => import("./screens/Home"));
+/* const Home = React.lazy(() => import("./screens/Home")); */
 
-const Master = styled.div`
+/* const Master = styled.div`
   padding-top: 60px;
 `;
-
+ */
 const AppRouter = () => {
   return (
     <Router>
